1d7f4752a0297229109477239a500b02660274d7
tyler621 <tyler.franzen@trojans.dsu.edu>
2022-06-03 15:20:39 -0500

    Add project files.

---
+++ Bowling/Bowling/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Bowling.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Bowling.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
